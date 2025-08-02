1de332c867bcc79e7e550d96cf968d937d075cea
LaboneeAktar <labonipub@gmail.com>
2025-08-02 16:47:12 +0600

    Created The digital Tribune project and set up

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <h1>Hi main</h1>
+      <h1>Hi main</h1>
+      <h1>Hi main</h1>
+    </div>
+  );
+};
+
+export default MainLayout;
