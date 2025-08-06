3a3558d5a96a59690c174518c524c746ecd0c7df
LaboneeAktar <labonipub@gmail.com>
2025-08-06 19:40:24 +0600

    Update LeftNavbar

--- src/components/Navbar/LeftNavbar.jsx
+++ src/components/Navbar/LeftNavbar.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { FaRegCalendarAlt } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
 const LeftNavbar = () => {
@@ -10,6 +11,31 @@ const LeftNavbar = () => {
       .then((data) => setCategories(data.data.news_category))
       .catch((error) => console.error(error));
   }, []);
+
+  // Dummy Design
+  const ads = [
+    {
+      id: 1,
+      title: "Bangladesh wins fifth Test by six runs in thrilling Oval finale",
+      category: "Cricket",
+      date: "August 4, 2025",
+      img: "https://i.ibb.co.com/G40YZnhK/5494.jpg",
+    },
+    {
+      id: 2,
+      title: "India's nightwatchman Deep stuns England with gritty fifty",
+      category: "Sports",
+      date: "August 2, 2025",
+      img: "https://i.ibb.co.com/5hLPwc6Y/soccer-players-action-professional-stadium.jpg",
+    },
+    {
+      id: 3,
+      title: "Mohammed Siraj leads India to dramatic victory over England",
+      category: "Cricket",
+      date: "August 5, 2025",
+      img: "https://i.ibb.co.com/CKB66dfz/5482.jpg",
+    },
+  ];
 
   return (
     <div>
@@ -34,6 +60,25 @@ const LeftNavbar = () => {
               </span>
             )}
           </NavLink>
+        ))}
+      </div>
+
+      <div className="py-12">
+        {ads.map((item) => (
+          <div key={item.id} className="flex flex-col gap-3 mb-4 items-start">
+            <img
+              src={item.img}
+              alt={item.title}
+              className="w-full h-36 rounded-lg object-cover"
+            />
+            <div>
+              <h3 className="font-semibold text-sm">{item.title}</h3>
+              <div className="flex items-center text-gray-500 text-xs mt-1">
+                <span className="mr-3">{item.category}</span>
+                <FaRegCalendarAlt className="mr-1" /> {item.date}
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     </div>
