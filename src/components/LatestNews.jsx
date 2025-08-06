3bf81026515b12f26f5fa85cdf7c3ee7e12ac59b
LaboneeAktar <labonipub@gmail.com>
2025-08-06 15:11:10 +0600

    Update Latest News

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,24 +1,38 @@
+import { useContext, useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../provider/AuthContext";
 
 const LatestNews = () => {
+  const [news, setNews] = useState([]);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    fetch("https://openapi.programming-hero.com/api/news/category/01")
+      .then((res) => res.json())
+      .then((data) => setNews(data.data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div className="bg-gray-200 mt-7 mx-auto rounded-sm">
       <div className="flex gap-2 items-center justify-center p-3">
         <p className="py-2 px-6 bg-[#D72050] text-white rounded-sm">Latest</p>
-        <Marquee className="space-x-10" pauseOnHover={true}>
-          <Link to="/news">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-            provident amet unde temporibus cum.
-          </Link>
-          <Link to="/news">
-            Fugit neque ducimus, quam alias nulla repudiandae. A saepe eligendi
-            recusandae repudiandae quaerat rerum magnam.
-          </Link>
-          <Link to="/news">
-            Animi numquam doloremque sunt quae? Impedit, modi. Iusto,
-            dignissimos sint?
-          </Link>
+        <Marquee className="" pauseOnHover={true}>
+          {news.map((singleNews) => (
+            <div key={singleNews._id}>
+              {" "}
+              {user ? (
+                <Link to={`/news/${singleNews._id}`} className="mr-10">
+                  {singleNews.title}
+                </Link>
+              ) : (
+                <Link to="/login" className="mr-10">
+                  {singleNews.title}
+                </Link>
+              )}
+            </div>
+          ))}
         </Marquee>
       </div>
     </div>
